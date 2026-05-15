2e37908728778fb0d5f5a849113ffdd6bbaeeecc
Arthu34345 <aaa020707@mail.ru>
2026-05-15 03:23:11 +0300

    feat(ux): добавлен функционал элементов управления
    - Добавлен функционал кнопок пресетов массива
    
    - Добавлен функционал ползунку и полю выбора размера массива
    
    - Реализовано изменение текста кнопки "Старт" во время сортировки
    
    - Тип коллекции массива изменён на ObservableCollection
    
    - Переименован метод запуска сортировки

--- ViewModels/SortingViewModel.cs
+++ ViewModels/SortingViewModel.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
+using Avalonia.Controls;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using SortingVisualizer.Models;
@@ -13,23 +15,121 @@ public partial class SortingViewModel : ObservableObject
     public List<SortAlgorithmBase> SortAlgorithms { get; } = [new BubbleSort(), new BubbleSort()];
     private readonly Random _random = new();
     
-    [ObservableProperty] private List<SortingBar> _array;
+    [ObservableProperty] private ObservableCollection<SortingBar> _array;
     [ObservableProperty] private SortAlgorithmBase? _selectedAlgorithm;
-    [ObservableProperty] private bool _isSortAvailable = true;
+    [ObservableProperty] private int _arraySize = 20;
     [ObservableProperty] private int _delay = 10;
+    [ObservableProperty] private string _startStopButtonText = "Старт";
+    
+    [NotifyCanExecuteChangedFor(nameof(StartStopSortingCommand))]
+    [NotifyCanExecuteChangedFor(nameof(ShakeArrayCommand))]
+    [NotifyCanExecuteChangedFor(nameof(SetReversedArrayCommand))]
+    [NotifyCanExecuteChangedFor(nameof(SetCraterArrayCommand))]
+    [NotifyCanExecuteChangedFor(nameof(SetPyramidArrayCommand))]
+    [NotifyCanExecuteChangedFor(nameof(SelectAlgorithmCommand))]
+    [ObservableProperty] private bool _isSortAvailable = true;
     
     public SortingViewModel()
     {
         SelectAlgorithm(SortAlgorithms[0]);
-        Array = GenerateArray(20);
+        Array = GenerateSortedArray();
         ShakeArray();
     }
 
-    private List<SortingBar> GenerateArray(int size)
+    partial void OnIsSortAvailableChanged(bool value)
     {
-        List<SortingBar> newArray = new List<SortingBar>();
+        StartStopButtonText = value ? "Старт" : "Стоп";
+    }
+    
+    partial void OnArraySizeChanged(int value)
+    {
+        Array = GenerateSortedArray();
+        ShakeArray();
+    }
+
+    [RelayCommand(CanExecute = nameof(IsSortAvailable))]
+    private void SetCraterArray()
+    {
+        Array = GenerateCraterArray();
+    }
+    
+    [RelayCommand(CanExecute = nameof(IsSortAvailable))]
+    private void SetReversedArray()
+    {
+        Array = GenerateReversedArray();
+    }
+    
+    [RelayCommand(CanExecute = nameof(IsSortAvailable))]
+    private void SetPyramidArray()
+    {
+        Array = GeneratePyramidArray();
+    }
+    
+    private ObservableCollection<SortingBar> GenerateCraterArray()
+    {
+        /*Лучше заполнять массив через два указателя, вместо использования Insert для коллекции, ибо в таком случае
+         приходится проходиться по массиву каждый раз при вставке, n превращается в n^2*/
+        SortingBar[] newArray = new SortingBar[ArraySize]; 
         
-        for (int i = 1; i <= size; i++)
+        int left = 0, right = ArraySize - 1; 
+
+        for (int i = ArraySize; i >= 1; i--)
+        {
+            if (i % 2 == 0)
+            {
+                newArray[left] = new SortingBar(i);
+                left++;
+            }
+            else
+            {
+                newArray[right] = new SortingBar(i);;
+                right--;
+            }
+        }
+
+        return new ObservableCollection<SortingBar>(newArray);
+    }
+    
+    private ObservableCollection<SortingBar> GeneratePyramidArray()
+    {
+        //То же, что для GenerateCraterArray()
+        SortingBar[] newArray = new SortingBar[ArraySize];
+        int left = 0, right = ArraySize - 1;
+
+        for (int i = 1; i <= ArraySize; i++)
+        {
+            if (i % 2 == 0)
+            {
+                newArray[left] = new SortingBar(i);
+                left++;
+            }
+            else
+            {
+                newArray[right] = new SortingBar(i);;
+                right--;
+            }
+        }
+
+        return new ObservableCollection<SortingBar>(newArray);
+    }
+    
+    private ObservableCollection<SortingBar> GenerateReversedArray()
+    {
+        ObservableCollection<SortingBar> newArray = new ObservableCollection<SortingBar>();
+        
+        for (int i = ArraySize; i >= 1; i--)
+        {
+            newArray.Add(new SortingBar(i));
+        }
+
+        return newArray;
+    }
+
+    private ObservableCollection<SortingBar> GenerateSortedArray()
+    {
+        ObservableCollection<SortingBar> newArray = new ObservableCollection<SortingBar>();
+        
+        for (int i = 1; i <= ArraySize; i++)
         {
             newArray.Add(new SortingBar(i));
         }
@@ -40,19 +140,19 @@ public partial class SortingViewModel : ObservableObject
     [RelayCommand(CanExecute = nameof(IsSortAvailable))]
     private void ShakeArray()
     {
-        List<SortingBar> shakedArray = new List<SortingBar>(Array);
+        ObservableCollection<SortingBar> shakedArray = new ObservableCollection<SortingBar>(Array);
         
         for (int i = 0; i < shakedArray.Count; i++)
         {
-            int j = _random.Next(shakedArray.Count - 1);
+            int j = _random.Next(shakedArray.Count);
             (shakedArray[i].Value, shakedArray[j].Value) = (shakedArray[j].Value, shakedArray[i].Value);
         }
 
         Array = shakedArray;
     }
 
-    [RelayCommand(CanExecute = nameof(IsSortAvailable))]
-    private async Task StartSorting()
+    [RelayCommand]
+    private async Task StartStopSorting()
     {
         IsSortAvailable = false;
         await SelectedAlgorithm!.Sort(this);
