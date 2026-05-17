6155c5014e412de01ce732f894693814037badb4
Arthu34345 <aaa020707@mail.ru>
2026-05-17 14:02:00 +0300

    refactor(view): разделение 'божественного' MainWindow.axaml на отдельные UserControl

---
+++ Views/Controls/HeaderPanel.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SortingVisualizer.Views.Controls;
+
+public partial class HeaderPanel : UserControl
+{
+    public HeaderPanel()
+    {
+        InitializeComponent();
+    }
+}
