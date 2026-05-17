c3843100f35ac1d918456add2d19261bab7794e7
Arthu34345 <aaa020707@mail.ru>
2026-05-17 04:14:24 +0300

    feat(algos): добавлена быстрая сортировка

--- ViewModels/SortingViewModel.cs
+++ ViewModels/SortingViewModel.cs
@@ -13,7 +13,7 @@ namespace SortingVisualizer.ViewModels;
 
 public partial class SortingViewModel : ObservableObject, ISortingContext
 {
-    public List<SortAlgorithmBase> SortAlgorithms { get; } = [new BubbleSort(), new ShakerSort(), new BogoSort()];
+    public List<SortAlgorithmBase> SortAlgorithms { get; } = [new BubbleSort(), new ShakerSort(), new QuickSort(), new BogoSort()];
     public CancellationToken SortingCts
     {
         get => _sortingCts.Token;
